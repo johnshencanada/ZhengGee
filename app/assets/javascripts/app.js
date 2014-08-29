995a58776d13f237fe0bf604bf546f906d6f9244
john shen <johnshencanada@gmail.com>
2014-08-28 18:50:35 -0700

    testing123

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,16 +2,60 @@ $(window).scroll(function(event) {
 	var topPosition = $(window).scrollTop();
 	console.log(topPosition);
 
+	if (topPosition >= 750  && topPosition < 900) {
+		var degrees = (900 - topPosition)/100;
+		if (degrees >= 0.6) { 
+			scale(degrees,degrees);
+		}
+	}
+
+	if (topPosition >= 840  && topPosition < 900) {
+		var degrees = (topPosition - 840)*2.5;
+		rotate_bulb (-degrees);
+	}
+
+
 	if (topPosition >= 1300) {
-		var degress = topPosition - 1300;
-		rotate(degress);
+		var degrees = topPosition - 1300;
+		rotate(degrees);
 	}
 
 	if (topPosition >= 2196) {
 		$(".status-bar").addClass('animate');
 	}
 
-	if (topPosition < 2196) {
+	function scale(x, y) {
+		$("#lightbulb").css({
+		  '-webkit-transform' : 'scale('+degrees+','+degrees+')',
+		     '-moz-transform' : 'scale('+degrees+','+degrees+')',  
+		      '-ms-transform' : 'scale('+degrees+','+degrees+')',  
+		       '-o-transform' : 'scale('+degrees+','+degrees+')',  
+		          'transform' : 'scale('+degrees+','+degrees+')',  
+		               'zoom' : 1
+		});
+	}
+
+	function rotate_bulb (degrees) {
+		$("#lightbulb").css({
+		  '-webkit-transform' : 'rotate('+degrees+'deg)',
+		     '-moz-transform' : 'rotate('+degrees+'deg)',  
+		      '-ms-transform' : 'rotate('+degrees+'deg)',  
+		       '-o-transform' : 'rotate('+degrees+'deg)',  
+		          'transform' : 'rotate('+degrees+'deg)',  
+		               'zoom' : 1
+		});
+	}
+
+	function move (degrees) {
+		console.log("Moving");
+		$("#lightbulb").css({
+		  '-webkit-transform' : 'rotate('+degrees+'deg)',
+		     '-moz-transform' : 'rotate('+degrees+'deg)',  
+		      '-ms-transform' : 'rotate('+degrees+'deg)',  
+		       '-o-transform' : 'rotate('+degrees+'deg)',  
+		          'transform' : 'rotate('+degrees+'deg)',  
+		               'zoom' : 1
+		});
 	}
 
 	function rotate (degrees) {
@@ -25,5 +69,6 @@ $(window).scroll(function(event) {
 		               'zoom' : 1
 		});
 	}
+
 });
 
